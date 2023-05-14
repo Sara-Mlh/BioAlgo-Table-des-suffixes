b1f5772ad91893b3d160059ac7ba1bae68c84665
Sara-Mlh <sara.lyna26@yahoo.fr>
2023-05-14 12:44:38 +0100

    HTR

--- test_projet.py
+++ test_projet.py
@@ -5,13 +5,13 @@ def TABSUFF(chaine):
     for i in range(len(chaine)):
        suffixes.append(chaine[i:])
        indexes.append(i)
+    suffixes = sorted(suffixes)
     indexes.sort(key=lambda x: chaine[x:])
-    return indexes#,suffixes
+    return indexes,suffixes
 
 #QUESTION 2 : RECHERCHE DU MOTIF 
 def search_pattern(text,pattern):    
-    #Tabsuffix,_ = TABSUFF(text)  
-    Tabsuffix = TABSUFF(text)
+    _,Tabsuffix = TABSUFF(text)  
     indexes = []
     for i,suffix in enumerate(Tabsuffix):
         if pattern == suffix[:len(pattern)]:
@@ -25,22 +25,24 @@ def prefix_commun(word1,word2):
     l1 = len(word1)
     l2 = len(word2)
     i = 0
+    long = ""
     while i<min(l1,l2) and word1[i]==word2[i] :
+        long += word1[i]
         i+=1
-    return word1[:i]
+    return long  #word1[:i]
 
 def HTR(text):
-    suffixes,_ = TABSUFF(text)
+    indexsuff ,suffixes = TABSUFF(text)
+    n = len(text)
     htr_table = []
     htr_len = []
-    n = len(suffixes)
 
     for i in range(1,n):
         suffix1 = suffixes[i]
         suffix2 = suffixes[i-1]
-        
         htr_table.append(prefix_commun(suffix1,suffix2))
-        htr_len.append(len(prefix_commun(suffix1,suffix2)))
+        htr_len.append(len(prefix_commun(suffix1, suffix2)))
+    
 
     return htr_table,htr_len
 
@@ -52,13 +54,13 @@ mot1 = "AABBCZZZZ"
 motif = "ABBC"
 word ="banana"
 
-#print("Table des suffixes: \n",TABSUFF(word))
+print("Table des suffixes : \n",TABSUFF(word))
+#print("Le motif se trouve aux indices :",search_pattern(mot,motif))
+# print("longest prefix :",prefix_commun(mot,mot1))
 
-#print("Table htr : \n",HTR(word))
+print("Table htr : \n",HTR(word))
 
-print("longest prefix \n",prefix_commun(mot,mot1))
 
 #print(mot[0:5])
 
-#print("Table des suffixes: \n",TABSUFF(mot))
 #print('les indices sont :',search_pattern(mot,motif))
