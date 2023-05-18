9831893b786c7f80f6393494c9b119d72128559d
Sara-Mlh <sara.lyna26@yahoo.fr>
2023-05-18 03:24:12 +0100

    Final

--- test_projet.py
+++ test_projet.py
@@ -2,121 +2,128 @@ import time
 import random
 import string
 import matplotlib.pyplot as plt
+import pandas as pd
 #QUESTION 1 : TABLE DE SUFFIXES
 def TABSUFF(chaine):
-    indexes=[]
+    indexes = []
     suffixes = []
     for i in range(len(chaine)):
-       suffixes.append(chaine[i:])
-       indexes.append(i)
-    suffixes = sorted(suffixes)
-    indexes.sort(key=lambda x: chaine[x:])
-    return indexes,suffixes
+        suffixes.append(chaine[i:])
+        indexes.append(i)
+    suffixes = sorted(suffixes) #suffixes contient les suffixes eux meme
+    indexes.sort(key=lambda x: chaine[x:])  #indexes contient les indices des suffixes dans le texte
+    return indexes, suffixes
 
 #QUESTION 2 : RECHERCHE DU MOTIF 
-def search_pattern(text,pattern):    
-    _,Tabsuffix = TABSUFF(text)  
+def search_pattern(text, pattern):
+    Tabsuffix = TABSUFF(text)[1]
     indexes = []
     for suffix in enumerate(Tabsuffix):
-        if pattern == suffix[:len(pattern)]:
-            index = len(text) - len(suffix)
-            indexes.append(index)
+        if pattern == suffix[1][:len(pattern)]:
+            index = len(text) - len(suffix[1])
+            indexes.append(index)      #ajouer l occurence i a la liste indexes
     return indexes
+
 
 #QUESTION 3 : HTR TABLE
 
-def prefix_commun(word1,word2):
+def prefix_commun(word1, word2):
     l1 = len(word1)
     l2 = len(word2)
     i = 0
     long = ""
-    while i<min(l1,l2) and word1[i]==word2[i] :
+    while i < min(l1, l2) and word1[i] == word2[i]:
         long += word1[i]
-        i+=1
-    return long  
+        i += 1
+    return long
+
 
 def HTR(text):
-    _,suffixes = TABSUFF(text)
-    htr_table = [] 
+    _, suffixes = TABSUFF(text)
+    htr_table = []
     htr_len = []
-    for i in range(1,len(suffixes)):
+    for i in range(1, len(suffixes)):
         suffix1 = suffixes[i]
-        suffix2 = suffixes[i-1]
-        htr_table.append(prefix_commun(suffix1,suffix2)) #htr table contains strings of prefixes
-        htr_len.append(len(prefix_commun(suffix1, suffix2))) # htr len is the real one and contain length of prefixes
-    #htr_len.append(0)
-    return htr_table,htr_len
+        suffix2 = suffixes[i - 1]
+        htr_table.append(prefix_commun(suffix1, suffix2))  # htr table contient les préfixes communs
+        htr_len.append(len(prefix_commun(suffix1, suffix2)))  # htr len est la reel et qui contient le len des prefixes
+    return htr_table, htr_len
 
-#QUESTION 4 TS ET HTR
 
-#LONGEST REPEATED FACTORS 
+#QUESTION 4: LONGEST REPEATED FACTORS
 def longest_repeated_factors(text):
-    _,htr = HTR(text)
+    _, htr = HTR(text)
     max_htr = max(htr)
-    suffixes,_ = TABSUFF(text)
+    suffixes, _ = TABSUFF(text)
     factors = []
-    #get the suffixes indexes of the max htr from the htr table
-    for i in range(len(htr)) :
-        if htr[i] == max_htr :
+    # get the suffixes indexes of the max htr from the htr table
+    for i in range(len(htr)):
+        if htr[i] == max_htr:
             factor = text[suffixes[i]: suffixes[i] + max_htr]
             if factor not in factors:
                 factors.append(factor)
     return factors
-#FACTORS REPEATED AT LEAST 3 TIMES
+
+
+#QUESTION 5: FACTORS REPEATED AT LEAST 3 TIMES
 def repeated_factors3(text):
-    _,htr = HTR(text)
-    suffixes,_ = TABSUFF(text)
+    _, htr = HTR(text)
+    suffixes, _ = TABSUFF(text)
     factors = []
-    repeated_factors= []
-    #get the suffixes indexes from the htr table
-    for i in range(len(htr)) :
-        if htr[i] >= 2:
-            factors.append(text[suffixes[i]:suffixes[i] + htr[i]])
-    #save the factors that are repeated at least three times 
-    for factor in factors :
-        count = text.count(factor)
-        if count >=3 and factor not in repeated_factors :
-            repeated_factors.append(factor)      
+    repeated_factors = []
+    for i in range(len(htr)):
+        if htr[i] >= 3:
+            factor = text[suffixes[i]: suffixes[i] + htr[i]]
+            factors.append(factor)
+    for factor in factors:
+        count = factors.count(factor)
+        if count >= 3 and factor not in repeated_factors: # si le nombre de repetitions count est minimum 3 alors ajouter l elemnt a la liste des facteurs
+            repeated_factors.append(factor)
     return repeated_factors
-#QUESTION 5 INVERSE SUFFIX TABLE   
+
+
+#QUESTION 6: INVERSE SUFFIX TABLE
 def Inverse_TS(text):
-    suffixes,_ = TABSUFF(text)
+    suffixes, _ = TABSUFF(text)
     inverse = [0] * len(suffixes)
     for i in range(len(suffixes)):
         index = suffixes[i]
         inverse[index] = i
     return inverse
 
-#QUESTION 6 SHORTEST UNIQUE FACTOR
+
+#QUESTION 7: SHORTEST UNIQUE FACTOR
 def Shortest_unique_factors(text):
-    _,htr = HTR(text)
-    suffixes,_ = TABSUFF(text)
+    _, htr = HTR(text)
+    suffixes, _ = TABSUFF(text)
     factors = []
-    for i in range(1,len(htr)):
-        factor = text[suffixes[i]:suffixes[i] + htr[i]]
-        if htr[i]> 0 and text.count(factor) == 1 : 
-            if not factors :
-               factors.append(factor)
-            else :
-                if factor not in factors :
-                    factors.append(factor)
-    if factors :
-      min_length = len(min(factors, key=len))
-      shortest = [factor for factor in factors if len(factor) == min_length]
-      return shortest
-    else :
+    for i in range(1, len(htr)):
+        factor = text[suffixes[i]: suffixes[i] + htr[i]]
+        if htr[i] > 0 and text.count(factor) == 1:
+            if not factors:
+                factors.append(factor)
+            else:
+                if factor not in factors:
+                    factors.append(factor)  # ajouter a la liste des facteurs uniques
+    if factors:
+        min_length = len(min(factors, key=len))  #la longeur minimale d'un facteur de la liste
+        shortest = [factor for factor in factors if len(factor) == min_length]  #ajouter tout les elements qui ont la mm longeur minimale
+        return shortest
+    else:
         return []
-#QUESTION 7 SUPERMAXIMAL FACTOR
+
+
+#QUESTION 8: SUPERMAXIMAL FACTORS
 def find_supermaximal_repetitions(text):
-    _, lcp = HTR(text)  # Fonction pour construire les tables LCP et suffixes
-    suffixes,_=TABSUFF(text)
+    _, lcp = HTR(text)
+    suffixes, _ = TABSUFF(text)
     n = len(text)
     repetitions = []
     for i in range(1, n):
         if i < len(lcp):  # Vérifier si l'indice est valide
             lcp_length = lcp[i]  # Longueur du préfixe commun avec le suffixe précédent
             suffix_i = suffixes[i]  # Indice du suffixe courant
-            factor = text[suffix_i:suffix_i + lcp_length]  # Facteur correspondant au préfixe commun
+            factor = text[suffix_i: suffix_i + lcp_length]  # Facteur correspondant au préfixe commun
             repetition_count = text.count(factor)
             if repetition_count > 1:
                 is_supermaximal = True
@@ -128,7 +135,8 @@ def find_supermaximal_repetitions(text):
                     repetitions.append(factor)
     return repetitions
 
-#QUESTION 8 LONGEST COMMUN FACTOR BETWEEN TWO WORDS
+
+#QUESTION 9: LONGEST COMMON FACTOR BETWEEN TWO WORDS
 def Longest_common_factor(T1, T2):
     # Concatenate the texts with a unique separator
     concat_text = T1 + '#' + T2
@@ -137,7 +145,7 @@ def Longest_common_factor(T1, T2):
     suffixes, _ = TABSUFF(concat_text)
 
     # Calculate the LCP table for the concatenated text
-    _,lcp = HTR(concat_text)
+    _, lcp = HTR(concat_text)
 
     max_lcp = 0  # Maximum LCP value
     common_suffix_index = -1  # Index of the common suffix in the concatenated text
@@ -157,30 +165,31 @@ def Longest_common_factor(T1, T2):
 
     return longest_common_factor
 
+
 #********************* TEST *************************
       #0123456789
-mot = "AABBCZCAABBC"
-mot1 = "AABBCZZZZ"
-motif = "ABBC"
-word ="saralynalynasara"
-pattern = "sara"
+word = "mississippi"
+word1 ="rississippi"
+pattern = "ssi"
+ts,_= TABSUFF(word)
+_,htr = HTR(word)
 
-print("The suffix table of the word {} is : \n {}".format(word,TABSUFF(word)))
-print("The pattern {} is in the positions : {}".format(pattern,search_pattern(word,pattern)))
-# print("longest prefix :",prefix_commun(mot,mot1))
+print("\nWord :",word)
+print("\nPattern :",pattern)
+print("\nThe suffix table of the word {} is : \n {}".format(word,ts))
+print("\nThe pattern {} is in the positions : {}".format(pattern,search_pattern(word,pattern)))
 
-print("Table htr of {} is :\n {} ".format(word,HTR(word)))
+print("\nTable htr of {} is :\n {} ".format(word,htr))
 
-print("Longest factors of {} are: {}".format(word,longest_repeated_factors(word)))
+print("\nLongest factors of {} are: {}".format(word,longest_repeated_factors(word)))
 
-print("The factors that are repeated at least 3 times in the word {} are: {}".format(word,repeated_factors3(word)))
+print("\nThe factors that are repeated at least 3 times in the word {} are: {}".format(word,repeated_factors3(word)))
 
-print("The inverse of suffix table of the word {} is : \n {}".format(word,Inverse_TS(word)))
+print("\nThe inverse of suffix table of the word {} is : \n {}".format(word,Inverse_TS(word)))
 #print(mot[0:5])
-print("The shortest unique factors of the word {} are : {}".format(word,Shortest_unique_factors(word)))
-print('les indices sont :',search_pattern(mot,motif))
-print("The super maximales of the word {} are : {}".format(word,find_supermaximal_repetitions(word)))
-print("The longest commun factor words {}  and {} are : {}".format(word,pattern,Longest_common_factor(word,pattern)))
+print("\nThe shortest unique factors of the word {} are : {}".format(word,Shortest_unique_factors(word)))
+print("\nThe super maximales of the word {} are : {}".format(word,find_supermaximal_repetitions(word)))
+print("\nThe longest commun factor words {}  and {} are : {}".format(word,pattern,Longest_common_factor(word,word1)))
 
 #Test de temps d execution
 #Fonction qui genere un text d une taille donnée
@@ -238,6 +247,62 @@ for size in text_sizes:
     print(f"Temps d'exécution de fonction6 : {time_func6} secondes")
     print(f"Temps d'exécution de fonction7 : {time_func7} secondes")
 
+text_sizes = [100, 200, 600, 1000, 2000, 5000]  # Variable text sizes to test
+
+# Create a dictionary to store the execution times for each function
+execution_times = {
+    'Table suffixes TS': [],
+    'Table HTR': [],
+    'PlusLongsFacteursRépetes': [],
+    'Repeated_Factors3': [],
+    'Inverse TS': [],
+    'Courts Facteurs uniques': [],
+    'Repetitions Supermximales': []
+}
+
+# Iterate over each text size
+for size in text_sizes:
+    text = generate_text(size)  # Generate text of size 'size'
+
+    # Measure the execution time for each function
+    execution_times['Table suffixes TS'].append(measure_execution_time(TABSUFF, text))
+    execution_times['Table HTR'].append(measure_execution_time(HTR, text))
+    execution_times['PlusLongsFacteursRépetes'].append(measure_execution_time(longest_repeated_factors, text))
+    execution_times['Repeated_Factors3'].append(measure_execution_time(repeated_factors3, text))
+    execution_times['Inverse TS'].append(measure_execution_time(Inverse_TS, text))
+    execution_times['Courts Facteurs uniques'].append(measure_execution_time(Shortest_unique_factors, text))
+    execution_times['Repetitions Supermximales'].append(measure_execution_time(find_supermaximal_repetitions, text))
+
+# Create a DataFrame for each function
+df_TS = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['Table suffixes TS']})
+df_HTR = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['Table HTR']})
+df_PLF = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['PlusLongsFacteursRépetes']})
+df_RF3 = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['Repeated_Factors3']})
+df_ITS = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['Inverse TS']})
+df_CFU = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['Courts Facteurs uniques']})
+df_RSM = pd.DataFrame({'Taille du texte': text_sizes, 'Temps d\'exécution': execution_times['Repetitions Supermximales']})
+
+# Print the DataFrames
+print("Table suffixes TS:")
+print(df_TS)
+
+print("Table HTR:")
+print(df_HTR)
+
+print("PlusLongsFacteursRépetes:")
+print(df_PLF)
+
+print("Repeated_Factors3:")
+print(df_RF3)
+
+print("Inverse TS:")
+print(df_ITS)
+
+print("Courts Facteurs uniques:")
+print(df_CFU)
+
+print("Repetitions Supermximales:")
+print(df_RSM)
 
 #Le plot de comparaison
 
@@ -255,3 +320,4 @@ plt.title('Comparaison des temps d\'exécution')
 plt.legend()
 
 plt.show()
+
